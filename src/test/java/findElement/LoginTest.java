d85a089b03c88263493479b41fb5342554fc3e4e
uyenit <thuyuyen098@gmail.com>
2020-10-24 15:59:16 +0700

    find element with selenium

--- src/test/java/findElement/LoginTest.java
+++ src/test/java/findElement/LoginTest.java
@@ -12,9 +12,11 @@ public class LoginTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/login");
 
+
         driver.findElement(By.id("username")).sendKeys("tomsmith");
+
         driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.xpath("//*[@type='submit']")).click();
+        driver.findElement(By.xpath("//button//*[contains(.,'Login')]")).click();
         Assert.assertEquals(driver.getCurrentUrl(),
                 "https://the-internet.herokuapp.com/secure",
                 "Login password successfully");
